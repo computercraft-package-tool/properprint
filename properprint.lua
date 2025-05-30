092bb3f0494426e239a3f0171ae8746dc5d12d89
PentagonLP <85071935+PentagonLP@users.noreply.github.com>
2025-05-30 14:14:42 +0200

    Major rewrite to get rid of recursion

--- properprint.lua
+++ properprint.lua
@@ -17,60 +17,73 @@ local function getLastCharPosition(string, char)
     return string.find(string, char .. "[^" .. char .. "]*$")
 end
 
---[[ Recursive helper function to split the input string into lines and print them
-    @param String text: The full string tp print
-    @param String inclstring: string consisting of spaces to provide initial indentation
-    @param Number xsize: The Number of characters that can fit in one line in the terminal
-    @param Boolean firstline: Whether the line to print is the first line of the string (used for indentation)
-]]
-local function printWithoutLinebreakHelper(text, inclstring, xsize, firstline)
-
-    -- Remaining text fits in one line?
-    if (#inclstring + #text) <= xsize then
-        print(inclstring .. text)
-        return
-    end
-
-    -- Get position for end of line
-    local lcp = getLastCharPosition(string.sub(text, 0, xsize - #inclstring)," ")
-    local cutposition
-    if lcp == nil then
-        cutposition = xsize
-    else
-        cutposition = #inclstring + lcp
-    end
-    
-    -- Print line
-    print(string.sub(inclstring .. text, 0, cutposition))
-    
-    -- All lines from second line onwards have one space more indentation
-    local inclstringnew
-    if firstline then
-        inclstringnew = inclstring .. " "
-    else 
-        inclstringnew = inclstring
-    end
-    
-    -- Recursive call to handle remaining Text
-    printWithoutLinebreakHelper(string.sub(text, cutposition + 1 - #inclstring), inclstringnew, xsize, false)
-end
-
 --[[ Print a string with correct linebreaking at spaces and custom indentation
     @param String text: The string to print
     @param Number incl: The default indentation, offset from the left edge of the terminal
 ]]
-function properprint.pprint(text, incl)
-    incl = incl or 0
+function properprint.pprint(text, defaultindentation, nextlineindendation)
+    defaultindentation = defaultindentation or 0
+    nextlineindendation = nextlineindendation or 1
 
     local xsize = term.getSize()
 
-    -- Build string consisting of spaces for indentation according to the 'incl' parameter
-    local inclstring = ""
-    for _ = 1, incl, 1 do
-        inclstring = " " .. inclstring
+    -- Build string consisting of spaces for indentation according to the 'defaultindentation' and 'nextlineindendation' parameter
+    local defaultindentationstring = ""
+    for _ = 1, defaultindentation, 1 do
+        defaultindentationstring = defaultindentationstring .. " "
     end
 
-    printWithoutLinebreakHelper(text, inclstring, xsize, true)
+    local nextlineindendationstring = ""
+    for _ = 1, nextlineindendation, 1 do
+        nextlineindendationstring = nextlineindendationstring .. " "
+    end
+
+    local firstline = true
+
+    -- For all terminal lines...
+    while not (#text == 0)  do
+
+        -- Use 'nextlineindendation' everywhere, exept the first line
+        local lineindentationstring = defaultindentationstring
+        if not firstline then
+            lineindentationstring = defaultindentationstring .. nextlineindendationstring
+        end
+
+        local lastcharinlineindex, firstcharinnextlineindex
+        local spacetofitin = xsize - #lineindentationstring
+        -- Find last space character in the terminal line, if there is one
+        local spacecharposition = getLastCharPosition(string.sub(text, 1, spacetofitin), " ")
+        --[[ If text fits in one line, or the first word (character sequence up to the first space) is
+            longer then the line, do a conventional linebreak at the end of the terminal line
+
+            -----spacetofitin----
+            abcdefghijklmnop
+                                ^^
+            -----spacetofitin----
+            abcdefghijklmnopqrstuvwxyz
+                                ^^
+        ]]
+        if (spacetofitin >= #text) or (spacecharposition == nil) then
+            lastcharinlineindex = spacetofitin
+            firstcharinnextlineindex = spacetofitin + 1
+        --[[ If text does not fit in one line, but contains a space character before the end of the
+            terminal line, break at the space
+
+            -----spacetofitin----
+            abcdef ghijkl mnop qrstuvw
+                             ^ ^
+        ]]
+        else
+            lastcharinlineindex = spacecharposition - 1
+            firstcharinnextlineindex = spacecharposition + 1
+        end
+
+        -- Print the first line and chop it off of the remaining text
+        print(lineindentationstring .. string.sub(text, 1, lastcharinlineindex))
+        text = string.sub(text, firstcharinnextlineindex)
+
+        firstline = false
+    end
 end
 
 return properprint
